7cc282e68fd96039609f062401e434a37af296bc
Gokul Vijayaraghavan <72010761+GokulVijayaraghavan@users.noreply.github.com>
2023-06-29 23:36:06 -0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-@app.route("/signup", methods=["POST"])
-def signup():
-    username = request.json.get("username")
-    password = request.json.get("password")
-    user_data = {"username": username, "password": password}
-    users.insert_one(user_data)
-    return jsonify({"message": "Signup successful"})
